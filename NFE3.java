2a2bc47baab3c24a3aa056406c10954a6e819433
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-08 15:09:45 +0530

    Create NFE3.java

---
+++ NFE3.java
@@ -0,0 +1,19 @@
+/* NumberFormatException */
+class NFE 
+{
+    public static void main(String[] args) {
+        
+        String str="abhi";
+        
+        try
+        {
+            int a=Integer.parseInt(str);
+            System.out.println(a);
+        }    
+        catch(NumberFormatException n)
+        {
+            System.out.println("String "+str+" can't be Converted to Integer");
+        }   
+        System.out.println("Main method ended"); 
+    }
+}
